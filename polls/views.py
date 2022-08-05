148969a65018b71613c9c5043176f7720fadc4b8
ahahn94 <ahahn94@outlook.com>
2022-08-05 16:46:59 +0200

    Add Unit Tests For Question Results View

--- polls/views.py
+++ polls/views.py
@@ -30,6 +30,10 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+    def get_queryset(self):
+        already_published_questions = Question.objects.filter(pub_date__lte=timezone.now())
+        return already_published_questions
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
